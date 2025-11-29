4673ef68541103b476680548c701873c502df142
AlexWS12 <alexwaisman12@gmail.com>
2025-11-28 21:16:32 -0500

    feat: add main fil conversion logic

--- converter.py
+++ converter.py
@@ -1,1 +1,22 @@
+from pathlib import Path
+from conversions import CONVERSIONS
 
+
+def normalize_ext(ext: str) -> str:
+    ext = ext.strip().lower() # checks for .ext format, if wrong, fixes it (lowercase and adds dot)
+    return "." + ext if not ext.startswith(".") else ext 
+
+
+def convert_all(root: str, from_ext: str, to_ext: str):
+    from_ext = normalize_ext(from_ext)
+    to_ext = normalize_ext(to_ext)
+
+    func = CONVERSIONS[(from_ext, to_ext)]  # get the conversion function
+
+    root_path = Path(root)
+
+    for path in root_path.rglob(f"*{from_ext}"):
+        if path.is_file():
+            dst = path.with_suffix(to_ext)
+            func(path, dst)
+            print(f"{path} → {dst}")
